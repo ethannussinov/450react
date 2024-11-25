2893c3dd6ccdde91fc88f864ae1a6e3b6b5c3293
Weikai Rao <weikairao@gmail.com>
2024-11-25 02:30:01 -0600

    Transfer previous basic STL map into this project

--- src/pages/HeatmapPage.js
+++ src/pages/HeatmapPage.js
@@ -1,10 +1,14 @@
 import React from "react";
+import StLouisMap from '../components/StLouisMap';
+import 'leaflet/dist/leaflet.css';
+import './HeatmapPage.css';
 
 const HeatmapPage = () => {
   return (
     <div className="container mt-4">
       <h1 className="text-center">Heatmap</h1>
       <p>View metrics across districts in a geographical heatmap.</p>
+      <StLouisMap />
     </div>
   );
 };
